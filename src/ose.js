09d2646b3b7911f33320f18f2f5031fe7b82f8cc
Osezele Orukpe <osezele.o@gmail.com>
2020-11-27 23:28:33 +0100

    first commit

---
+++ src/ose.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+    bio: "Hi, I'm Ose. I'm a backend/android software engineer based in Lagos, Nigeria. " +
+    "I currently work at Blueloop Inc (blueloop.app). " +
+    "I love building challenging APIs and reading about Android technologies.",
+    prompt: 'Want to know more about me?:',
+    links: [
+        {
+            name: 'GitHub',
+            value: 'https://github.com/Uncensored-Developer',
+        },
+        {
+            name: 'LinkedIn',
+            value: 'https://www.linkedin.com/in/osezele-orukpe-1a37a9167',
+        },
+        {
+            name: 'Twitter',
+            value: 'https://twitter.com/__warchief',
+        },
+        {
+            name: 'My Resume',
+            value: 'https://www.canva.com/design/DADxK8yD0v0/wl4GbAdUj4zD2my6111axg/view?utm_content=DADxK8yD0v0&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink',
+        },
+    ],
+    email: 'osezele.o@gmail.com'
+};
